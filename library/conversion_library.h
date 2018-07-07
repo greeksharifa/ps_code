f0b2c4803e35ce92793cad34dd5354a8fec66a2c
greeksharifa <greeksharifa.gmail.com>
2018-07-07 18:14:16 +0900

    tab fix

--- library/conversion_library.h
+++ library/conversion_library.h
@@ -3,9 +3,9 @@
 #include "re_define.h"
 
 vi string_to_vi(string& s) {
-	vi ret(s.size());
-	for (int i = 0; i < s.size(); i++)
-		ret[i] = s[i] - '0';
-	return ret;
+    vi ret(s.size());
+    for (int i = 0; i < s.size(); i++)
+        ret[i] = s[i] - '0';
+    return ret;
 }
 
