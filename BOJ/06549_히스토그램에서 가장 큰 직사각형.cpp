c61980cdea75f4c0ab1713f3cada64ae86f06155
greeksharifa <greeksharifa.gmail.com>
2018-07-07 22:12:23 +0900

    06549 히스토그램에서 가장 큰 직사각형

---
+++ BOJ/06549_히스토그램에서 가장 큰 직사각형.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<stack>
+
+#include "../library/re_define.h"
+
+using namespace std;
+
+int main_06549() {
+    int n;
+    while (true) {
+        scanf("%d", &n);
+        if (!n)break;
+        stack<pi> st;
+        ll ans = 0;
+        for (int i_index = 0; i_index <= n; i_index++) {
+            int i_height;
+            if (i_index < n)
+                scanf("%d", &i_height);
+            else
+                i_height = -1;
+            int left = i_index;
+            // top_index: st.top().first, top_height: st.top().second
+            while (!st.empty() && st.top().second > i_height) {
+                ans = max(ans, ((ll)i_index - st.top().first)*st.top().second);
+                left = st.top().first;
+                st.pop();
+            }
+            st.push(mp(left, i_height));
+        }
+        printf("%lld\n", ans);
+    }
+    return 0;
+}
